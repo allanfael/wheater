034251508ac228b2a929e176fc2bdf45affb859b
Allan Rafael <allan_fael@hotmail.com>
2019-06-27 14:44:46 -0300

    Update App.js

--- App.js
+++ App.js
@@ -25,7 +25,6 @@ export default class App extends Component {
 
   componentDidMount() {
     this.updateLocation('New York');
-    console.log(this.updateLocation('New York').toString());
   }
 
   updateLocation = async city => {
